50febace0df065fce5f2cd5e4134abccee41eb8e
GenuinelyMeizner <frederikmpetersen@gmail.com>
2021-10-04 13:30:10 +0200

    test

---
+++ uge36/script.py
@@ -0,0 +1,9 @@
+def msg(name):
+    print('Hello World')
+
+    if name == 'Frederik':
+        print(f'Hello {name}')
+    else:
+        print('Exterminate Exterminate Exterminate')
+
+msg('Frederik')
